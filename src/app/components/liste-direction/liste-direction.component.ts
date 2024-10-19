e55147b958742eea0032e8c9037b5b4cd4e61cfb
Fatoumata Ba <fatmataba0620@gmail.com>
2024-10-19 17:06:34 +0000

    comited
    direction

--- src/app/components/liste-direction/liste-direction.component.ts
+++ src/app/components/liste-direction/liste-direction.component.ts
@@ -1,32 +1,22 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
 import { Direction } from '../../models/direction.model';
 import { DirectionService } from '../../services/direction.service';
 
 @Component({
   selector: 'app-liste-direction',
   standalone: true,
-  imports: [CommonModule],
   templateUrl: './liste-direction.component.html',
   styleUrls: ['./liste-direction.component.css'],
+  imports:[CommonModule],
 })
 export class ListeDirectionComponent {
-  directions: Direction[] = [];
-  errorMessage: string | null = null;
-
-  constructor(private directionService: DirectionService) {}
+  
+  @Input()  directions: Direction[] = [];
+  @Input()  message:String='';
+  constructor() {}
 
   ngOnInit() {
-    this.directionService.getDirections().subscribe({
-      next: (data) => {
-        if (data.success) {
-          this.directions = data.directions; // Type compatible avec Direction[]
-        }
-      },
-      error: (error) => {
-        this.errorMessage = 'Erreur lors de la récupération des données';
-        console.error('Erreur:', error);
-      },
-    });
+    
   }
 }
