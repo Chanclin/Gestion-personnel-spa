15e37b58cf5de72cd8a0fe579abc7833f4b5bedc
LusaKessyPro <lusakessypro@gmail.com>
2024-10-24 20:13:12 +0200

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,5 +20,5 @@ ListeEntrepriseComponent
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'Gestion_Entreprise';
+  
 }
---
+++ src/app/page/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { ListeEntrepriseComponent } from '../../services/liste-entreprise/liste-entreprise.component';
+import { CreateEntrepriseComponent } from '../../create-entreprise/create-entreprise.component';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { DeleteEntrepriseComponent } from '../../delete-entreprise/delete-entreprise.component';
+import { ListeDirectionComponent } from '../../components/liste-direction/liste-direction.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [ListeEntrepriseComponent,CreateEntrepriseComponent,RouterOutlet,RouterLink,RouterLinkActive,FormsModule,DeleteEntrepriseComponent,ListeDirectionComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  title = 'Gestion_Entreprise';
+}
