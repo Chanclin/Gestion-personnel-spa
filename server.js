3cdfb9c55c1be8924e7fda6d830313bff3f0c578
Chancelin <chancelinmakosso@gmail.com>
2024-11-16 23:32:07 +0000

    g

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const path = require("path");
 
 const app = express();
 
-// Dossier statique de l'application Angular
-app.use(express.static(path.join(__dirname, "dist/gestion-personnel-spa")));
+// Modifiez le chemin pour pointer vers dist/gestion-personnel-spa/browser
+app.use(express.static(path.join(__dirname, "dist/gestion-personnel-spa/browser")));
 
-// Rediriger toutes les requêtes non trouvées vers Angular
+// Redirige toutes les requêtes vers index.html
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist/gestion-personnel-spa/index.html"));
+  res.sendFile(path.join(__dirname, "dist/gestion-personnel-spa/browser/index.html"));
 });
 
-// Démarrer l'application sur le port défini par Heroku ou 8080
+// Démarre le serveur sur le port Heroku ou 8080
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
