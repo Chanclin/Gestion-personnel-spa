b9018049d530a49b5fa836cd2d081c7b469fdc19
Chancelin <chancelinmakosso@gmail.com>
2024-11-22 14:21:54 +0000

    g

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production : false,
     // apiUrl : 'http://localhost:8080'
-    apiUrl : 'https://gestion-personnel-backend1-46bb8426e42a.herokuapp.com/'
+    apiUrl : 'https://gestion-personnel-backend1-46bb8426e42a.herokuapp.com'
     
 };
